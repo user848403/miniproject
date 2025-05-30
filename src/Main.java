90e870c2b196ac8d79216aa85e5a3bd342ef2c28
user848403 <159574414+user848403@users.noreply.github.com>
2025-05-30 23:24:20 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //mini project
+        Scanner sc =new Scanner (System.in);
+        int myNumber =(int)(Math.random()*100);
+        int guess=0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do{
+            System.out.println("guess number:");
+             guess= sc.nextInt();
+            if(guess==myNumber){
+                System.out.println("u are correct");
+                break;
+            }
+            else if (guess> myNumber){
+                System.out.println("your number is larger ");
+            }
+            else   {
+                System.out.println("your number is smaller ");
+            }
+        }while(guess>=0);
+
+        System.out.print("my number");
+        System.out.println(myNumber);
     }
 }
